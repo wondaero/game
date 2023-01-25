39ecba77c43daee2fb353bcb1f58a9a768a46243
Ro <wondaero@nate.com>
2023-01-26 00:09:51 +0900

    랭크 스테이지 추가 및 몇몇 UI 및 기능 변경

--- assets/js/common.js
+++ assets/js/common.js
@@ -10,7 +10,35 @@ const commonJs = {
     sessionStorage.accessKey = 'n.rmal';
     return true;
   },
-  isAccess: () => localStorage.kakaoLogin && JSON.parse(localStorage.kakaoLogin).id && sessionStorage.accessKey == 'n.rmal'
+  isAccess: () => localStorage.kakaoLogin && JSON.parse(localStorage.kakaoLogin).id && sessionStorage.accessKey == 'n.rmal',
+  stopDevToll: function(){
+    function detectDevTool(allow) {
+        if(isNaN(+allow)) allow = 100;
+        var start = +new Date(); // Validation of built-in Object tamper prevention.
+        debugger;
+        var end = +new Date(); // Validates too.
+        if(isNaN(start) || isNaN(end) || end - start > allow) {
+            window.location.reload();
+        }
+    }
+    if(window.attachEvent) {
+        if (document.readyState === "complete" || document.readyState === "interactive") {
+            detectDevTool();
+        window.attachEvent('onresize', detectDevTool);
+        window.attachEvent('onmousemove', detectDevTool);
+        window.attachEvent('onfocus', detectDevTool);
+        window.attachEvent('onblur', detectDevTool);
+        } else {
+            setTimeout(argument.callee, 0);
+        }
+    } else {
+        window.addEventListener('load', detectDevTool);
+        window.addEventListener('resize', detectDevTool);
+        window.addEventListener('mousemove', detectDevTool);
+        window.addEventListener('focus', detectDevTool);
+        window.addEventListener('blur', detectDevTool);
+    }
+  }
 }
 
 const qs = target => document.querySelector(target);
